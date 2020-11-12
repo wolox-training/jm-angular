7369490a8c74d64fabca8992c7bf14f118585001
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-12 11:07:24 -0300

    Auth Module lazy loading

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './screens/auth/auth.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: 'auth',
-    component: AuthComponent,
+    loadChildren: () => import('./screens/auth/auth.module').then((m) => m.AuthModule),
   },
   {
-    path: '',
+    path: '**',
     component: AppComponent,
   },
 ];
