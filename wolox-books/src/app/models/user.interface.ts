5fc0638a277f635156dfdec55f600a91c863c60b
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-12 11:10:52 -0300

    form-item and user interfaces

---
+++ wolox-books/src/app/models/user.interface.ts
@@ -0,0 +1,8 @@
+export default interface IUser {
+  email: string;
+  first_name: string;
+  last_name: string;
+  locale: string;
+  password: string;
+  password_confirmation: string;
+}
---
+++ wolox-books/src/app/screens/auth/models/form-item.interface.ts
@@ -0,0 +1,6 @@
+export default interface IFormItem {
+  id: string;
+  error?: string;
+  label: string;
+  type: string;
+}
