d5a2c4f8040dec6b4f11a4f4fbe7a2b1ac2e335a
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-13 09:18:56 -0300

    Import HttpClient Module and declare userService

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './screens/auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AuthModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AuthModule, HttpClientModule, AppRoutingModule],
+  providers: [UserService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
