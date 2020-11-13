31a845938af2a6db7cb5825ea0608d21cdaeb090
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-13 14:48:48 -0300

    User sessions management

--- wolox-books/src/app/services/user.service.ts
+++ wolox-books/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IFullUser, IEmailAndPassword, ILoggedUser } from '../models/user.interface';
 import { environment } from 'src/environments/environment';
@@ -10,8 +10,11 @@ import { environment } from 'src/environments/environment';
 })
 export class UserService {
   private readonly API_URI: string = `${environment.apiUri}`;
+  private isLoginSubject: BehaviorSubject<boolean>;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {
+    this.isLoginSubject = new BehaviorSubject<boolean>(this.hasToken());
+  }
 
   createUser(user: IFullUser): Observable<HttpResponse<ILoggedUser>> {
     return this.http.post<ILoggedUser>(this.API_URI + '/users', user, {
@@ -19,11 +22,35 @@ export class UserService {
     });
   }
 
+  hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.isLoginSubject.asObservable();
+  }
+
   login(credentials: IEmailAndPassword): Observable<string> {
     return this.http
       .post<IEmailAndPassword>(this.API_URI + '/users/sign_in', credentials, {
         observe: 'response',
       })
-      .pipe(map((response) => response.headers.get('access-token')));
+      .pipe(
+        map((response) => {
+          const accessToken = response.headers.get('access-token');
+          localStorage.setItem('token', accessToken);
+          this.updateSubject(true);
+          return accessToken;
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.updateSubject(false);
+  }
+
+  updateSubject(state: boolean): void {
+    this.isLoginSubject.next(state);
   }
 }
