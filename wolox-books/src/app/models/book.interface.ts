78a5495f68b595781c93ab29d0afdb8ecf25fdbf
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-17 14:24:31 -0300

    Get Books from API

---
+++ wolox-books/src/app/models/book.interface.ts
@@ -0,0 +1,11 @@
+export interface IBook {
+  author: string;
+  created_at: string;
+  editor: string;
+  genre: string;
+  id: number;
+  image_url: string;
+  title: string;
+  updated_at: string;
+  year: string;
+}
--- wolox-books/src/app/screens/auth/screens/book-list/book-list.component.ts
+++ wolox-books/src/app/screens/auth/screens/book-list/book-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BooksService } from 'src/app/services/books.service';
+import { Subscription } from 'rxjs';
+import { IBook } from 'src/app/models/book.interface';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
+  bookSubscription: Subscription;
+  books: Array<IBook>;
 
-  constructor() { }
+  constructor(private booksService: BooksService) {}
 
   ngOnInit(): void {
+    this.bookSubscription = this.booksService.getBooks().subscribe((response) => {
+      this.books = response.page;
+      console.log(this.books);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.bookSubscription.unsubscribe();
+  }
+
+  trackBook(book: IBook): number | undefined {
+    return book ? book.id : undefined;
+  }
 }
