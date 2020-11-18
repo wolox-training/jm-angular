081b2f206090a7608abd17692c44f32532d94387
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-18 10:26:50 -0300

    Pipe declaration

--- wolox-books/src/app/screens/auth/auth.module.ts
+++ wolox-books/src/app/screens/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { BookListComponent } from './screens/book-list/book-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchPipe } from 'src/app/pipes/search.pipe';
 
 @NgModule({
-  declarations: [AuthComponent, BookListComponent, NavbarComponent],
+  declarations: [AuthComponent, BookListComponent, NavbarComponent, SearchPipe],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
