91d3e56b2b34238e38bd3a28a537b53fcb1e8312
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-18 15:54:49 -0300

    Navigation trigger after click

--- wolox-books/src/app/screens/auth/screens/book-list/book-list.component.ts
+++ wolox-books/src/app/screens/auth/screens/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BooksService } from 'src/app/services/books.service';
 import { Subscription } from 'rxjs';
 import { IBook } from 'src/app/models/book.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
@@ -13,11 +14,11 @@ export class BookListComponent implements OnInit, OnDestroy {
   books: Array<IBook>;
   searchTitle = '';
 
-  constructor(private booksService: BooksService) {}
+  constructor(private booksService: BooksService, private router: Router) {}
 
   ngOnInit(): void {
     this.bookSubscription = this.booksService.getBooks().subscribe((response) => {
-      this.books = response.page;
+      this.books = response;
     });
   }
 
@@ -28,6 +29,10 @@ export class BookListComponent implements OnInit, OnDestroy {
   onKey(text: string): void {
     console.log(this.books);
     this.searchTitle = text;
+  }
+
+  openDetail(id: number): void {
+    this.router.navigate(['books/' + id]);
   }
 
   trackBook(book: IBook): number | undefined {
