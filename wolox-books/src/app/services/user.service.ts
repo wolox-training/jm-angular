edff76bf9172705859fc44a3f90cd3261fd3c991
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-18 09:29:09 -0300

    Change Interface name

--- wolox-books/src/app/services/user.service.ts
+++ wolox-books/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { IFullUser, IEmailAndPassword, ILoggedUser } from '../models/user.interface';
+import { IFullUser, IUserCredentials, ILoggedUser } from '../models/user.interface';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -19,9 +19,9 @@ export class UserService {
     });
   }
 
-  login(credentials: IEmailAndPassword): Observable<string> {
+  login(credentials: IUserCredentials): Observable<string> {
     return this.http
-      .post<IEmailAndPassword>(this.API_URI + '/users/sign_in', credentials, {
+      .post<IUserCredentials>(this.API_URI + '/users/sign_in', credentials, {
         observe: 'response',
       })
       .pipe(map((response) => response.headers.get('access-token')));
