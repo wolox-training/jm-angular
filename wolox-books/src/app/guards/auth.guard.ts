59551539597086c208b1ebb2536dd197f02a70ca
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-19 13:23:18 -0300

    Change canActivate for canLoad for modules

--- wolox-books/src/app/guards/auth.guard.ts
+++ wolox-books/src/app/guards/auth.guard.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, CanLoad } from '@angular/router';
 import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private userService: UserService, private router: Router) {}
 
   canActivate(): boolean {
+    const hasToken = this.userService.hasToken();
+    if (hasToken) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+  canLoad(): boolean {
     const hasToken = this.userService.hasToken();
     if (hasToken) {
       return true;
--- wolox-books/src/app/guards/unauth.guard.ts
+++ wolox-books/src/app/guards/unauth.guard.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, CanLoad } from '@angular/router';
 import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UnAuthGuard implements CanActivate {
+export class UnAuthGuard implements CanActivate, CanLoad {
   constructor(private userService: UserService, private router: Router) {}
 
   canActivate(): boolean {
+    const hasToken = this.userService.hasToken();
+    if (hasToken) {
+      this.router.navigate(['/books']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+  canLoad(): boolean {
     const hasToken = this.userService.hasToken();
     if (hasToken) {
       this.router.navigate(['/books']);
