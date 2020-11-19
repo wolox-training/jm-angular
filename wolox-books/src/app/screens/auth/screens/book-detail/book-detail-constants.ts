d1a82954111dab8eb1a8a061f4de932c2b631f9f
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-19 13:24:01 -0300

    Delete unnecesary constant

--- wolox-books/src/app/screens/auth/screens/book-detail/book-detail-constants.ts
+++ wolox-books/src/app/screens/auth/screens/book-detail/book-detail-constants.ts
@@ -1,13 +1,1 @@
-import { IBookDetail } from 'src/app/models/book.interface';
-
 export const BEST_SELLERS: string[] = ['John Ransom'];
-
-export const DETAIL: IBookDetail = {
-  author: '',
-  editor: '',
-  genre: '',
-  id: 1,
-  image_url: '',
-  title: '',
-  year: '',
-};
