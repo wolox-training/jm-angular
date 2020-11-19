87ef4114c3405bd8c66d80bf7c30fc73e36b5b0e
Joaquin Miguens <joaquin.miguens@wolox.com.ar>
2020-11-19 14:48:30 -0300

    Install ngRx/store dependencies and declare them in app.module

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UserService } from './services/user.service';
-import { AuthModule } from './screens/auth/auth.module';
-import { AuthGuard } from './guards/auth.guard';
 import { UnAuthModule } from './screens/unauth/unauth.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthModule } from './screens/auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+
+import { UserService } from './services/user.service';
+import { AuthGuard } from './guards/auth.guard';
 import { UnAuthGuard } from './guards/unauth.guard';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { BooksService } from './services/books.service';
 
+import { booksReducer } from './store/books.reducer';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UnAuthModule, AuthModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      books: booksReducer,
+    }),
+    UnAuthModule,
+    AuthModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   providers: [
     UserService,
     BooksService,
